d6f39ec71e2d964438524cf17d214fdf8a191dc5
Paulo <paulo70306@edu.unipar.br>
2024-05-09 07:35:28 -0300

    quinto commit?kkk

---
+++ Galeria de imagens/script.js
@@ -0,0 +1,13 @@
+console.log("----vai aparecer no inspecionar");
+
+    const teste = 50;
+    
+    let C = 0;
+    C= 10;
+    console.log(C);
+
+    document.addEventListener('DOMContentLoader', function () {
+        alert(123);
+
+    }
+)
