2cfc9abecc57c92b1b19346d7fbca6d62d7b915d
Paulo <paulo70306@edu.unipar.br>
2024-05-23 07:46:04 -0300

    aainsfiqnwe

--- Galeria de imagens/scripts-.js
+++ Galeria de imagens/scripts-.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){ 
 
     const baseImagens = {
-        todas: []
+        todas: ['imagem.jpg', 'cidade.png', 'cidade2.png', 'animais.png', 'animais2.png', 'natureza.png', 'natureza2.png'],
+        natureza: ['natureza.png', 'natureza2.png'],
+        cidade: ['cidade.png', 'cidade2.png'],
+        animais: ['animais.png', 'animais2.png'],
     }
+
+    function carregaImagens(){
+        // selecionar elementos html com base em um seletor
+        const galeria = document.querySelector("#galeria-imagens");
+
+        const imagens = baseImagens['todas'];
+
+        imagens.forEach(img => {
+            console.log(img);
+            galeria.innerHTML = '<div> <img src="imagens/'+img+'" /> </div>'
+        })
+    }
+
+    carregaImagens();
 
     // Evento de clique
     document.body.addEventListener('click', function (event){ 
