efd4e41c327e971fd79d229e3785d6e1a722a36f
Paulo <paulo70306@edu.unipar.br>
2024-05-24 09:28:40 -0300

    aainsfiqnwe532541

--- Galeria de imagens/scripts-.js
+++ Galeria de imagens/scripts-.js
@@ -1,32 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){ 
 
     const baseImagens = {
-        todas: ['imagem.jpg', 'cidade.png', 'cidade2.png', 'animais.png', 'animais2.png', 'natureza.png', 'natureza2.png'],
+        todas: ['cidade.png', 'cidade2.png', 'animais.png', 'animais2.png', 'animais3.png', 'animais4.png', 'natureza.png', 'natureza2.png'],
         natureza: ['natureza.png', 'natureza2.png'],
         cidade: ['cidade.png', 'cidade2.png'],
-        animais: ['animais.png', 'animais2.png'],
+        animais: ['animais.png', 'animais2.png', 'animais3.png', 'animais4.png',],
     }
 
-    function carregaImagens(){
+    function carregaImagens(categoria){
         // selecionar elementos html com base em um seletor
         const galeria = document.querySelector("#galeria-imagens");
-
-        const imagens = baseImagens['todas'];
+        galeria.innerHTML = '';
+        const imagens = baseImagens[categoria];
 
         imagens.forEach(img => {
             console.log(img);
-            galeria.innerHTML = '<div> <img src="imagens/'+img+'" /> </div>'
+            galeria.innerHTML += '<div> <img src="imagens/'+img+'" /> </div>'
+        
         })
     }
 
-    carregaImagens();
 
     // Evento de clique
     document.body.addEventListener('click', function (event){ 
 
         //acessa a lista de classe de onde clicou e procura pela classe botao-categoria
-        if(event.target.classList.contains("botao-categoria")){
-            alert("poggers");
+        if(event.target.classList.contains('botao-categoria')){
+            const categoria = event.target.dataset.categoria;
+            carregaImagens(categoria);
 
         }
 
